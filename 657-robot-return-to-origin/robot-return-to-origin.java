97c3db70ed9d8916eb697da6c49f5a43ba9d57bd
Abhishek Kumar Gupta <99818468+tle002@users.noreply.github.com>
2026-04-05 18:28:48 +0530

    Time: 8 ms (24.19%) | Memory: 45.2 MB (98.05%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0,y=0;
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='U')
+            y++;
+            else if(moves.charAt(i)=='D')
+            y--;
+            else if(moves.charAt(i)=='R')
+            x++;
+            else
+            x--;
+        }       
+        if(x==0&&y==0)
+        return true;
+        return false; 
+    }
+}
